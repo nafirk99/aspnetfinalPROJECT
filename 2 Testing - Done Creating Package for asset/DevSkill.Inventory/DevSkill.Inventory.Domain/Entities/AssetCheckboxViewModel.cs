723b08565772838b180fbee64929fa20dbbf33e4
nafirk99 <nafiz.ba.pakizatvl@gmail.com>
2024-10-21 14:07:27 +0600

    Done Creating Package which will contain Asset

---
+++ 2 Testing - Done Creating Package for asset/DevSkill.Inventory/DevSkill.Inventory.Domain/Entities/AssetCheckboxViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Domain.Entities
+{
+    public class AssetCheckboxViewModel
+    {
+        public int AssetId { get; set; }
+        public string AssetName { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public bool IsSelected { get; set; }  // To check if the asset is selected for this package
+    }
+}
