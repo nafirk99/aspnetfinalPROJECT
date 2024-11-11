e744cd3aa8eab9812a772b7815cbded6a79dce90
nafirk99 <nafiz.ba.pakizatvl@gmail.com>
2024-11-11 16:50:30 +0600

    DataTable integrated, testing phase

---
+++ 10 Rolebased Registration and extension method done class 28 done, test DataTable/DevSkill.Inventory/DevSkill.Inventory.Infrastructutre/Identity/ApplicationUserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace DevSkill.Inventory.Infrastructutre.Identity
+{
+    public class ApplicationUserLogin
+        : IdentityUserLogin<Guid>
+    {
+
+    }
+}
