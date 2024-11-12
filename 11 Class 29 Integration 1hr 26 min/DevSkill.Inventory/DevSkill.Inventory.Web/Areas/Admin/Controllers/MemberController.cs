7c3c7eafe0124296480332c6c5a8fef5ad79b874
nafirk99 <nafiz.ba.pakizatvl@gmail.com>
2024-11-12 17:21:44 +0600

    11 Class 29 Integration 1hr 26 min

---
+++ 11 Class 29 Integration 1hr 26 min/DevSkill.Inventory/DevSkill.Inventory.Web/Areas/Admin/Controllers/MemberController.cs
@@ -0,0 +1,43 @@
+﻿using DevSkill.Inventory.Infrastructutre.Identity;
+using DevSkill.Inventory.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevSkill.Inventory.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MemberController : Controller
+    {
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        public MemberController(RoleManager<ApplicationRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateRole()
+        {
+            var model = new RoleCreateModel();
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateRole(RoleCreateModel model)
+        {
+            if (ModelState.IsValid) 
+            {
+                await _roleManager.CreateAsync(new ApplicationRole
+                {
+                    Id = Guid.NewGuid(),
+                    NormalizedName = model.Name.ToUpper(),
+                    Name = model.Name,
+                    ConcurrencyStamp = DateTime.UtcNow.Ticks.ToString()
+                });
+            }
+            return View(model);
+        }
+    }
+}
