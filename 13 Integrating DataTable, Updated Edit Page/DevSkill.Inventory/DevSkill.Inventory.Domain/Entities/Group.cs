285ea53796086cc561ac71c19564247b135c8b54
nafirk99 <nafiz.ba.pakizatvl@gmail.com>
2024-11-17 17:33:46 +0600

    13 Integrating DataTable, Updated Edit Page

---
+++ 13 Integrating DataTable, Updated Edit Page/DevSkill.Inventory/DevSkill.Inventory.Domain/Entities/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Domain.Entities
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public String Name { get; set; } = "";
+
+        // Navigation Property For Related Products
+        public ICollection<Producta> Products { get; set; }
+    }
+}
---
+++ 13 Integrating DataTable, Updated Edit Page/DevSkill.Inventory/DevSkill.Inventory.Domain/Entities/Package.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Domain.Entities
+{
+    public class Package
+    {
+        public int Id { get; set; }   // Unique identifier for the package
+
+        [Required, MaxLength(100)]
+        public string PackageNumber { get; set; } = "";  // Unique number to identify the package
+
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation property for the one-to-many relationship with Producta (Assets)
+        public List<Producta> Productas { get; set; } = new List<Producta>();  // Collection of assets in this package
+    }
+}
