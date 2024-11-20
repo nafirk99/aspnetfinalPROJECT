f88b36738f1912b7f4f3325794b9ea7e34d452a4
nafirk99 <nafiz.ba.pakizatvl@gmail.com>
2024-11-20 13:24:48 +0600

    16 Advanced Search Integration  Fixed(20th Nov)

---
+++ 16 Advanced Search Integration  Fixed(20th Nov)/DevSkill.Inventory/DevSkill.Inventory.Web/Areas/Admin/Models/RoleChangeModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DevSkill.Inventory.Web.Areas.Admin.Models
+{
+    public class RoleChangeModel
+    {
+        public Guid UserId { get; set; }
+        public Guid RoleId { get; set; }
+
+        public SelectList? Users { get; set; }
+        public SelectList? Roles { get; set; }
+    }
+}
